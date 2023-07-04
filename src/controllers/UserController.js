a1749fd32f0239949da2210706ce75ca92376b1d
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-04 19:55:19 -0400

    Adicionado codigo para validacao do campo Name, do metodo de cadastro de um novo User.

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,7 +1,35 @@
 class UserController {
+  isValidName(name) {
+    let itsValidPT_BR = validator.isAlpha(name, ['pt-BR'], {
+      ignore: ' \''
+    })
+    let itsValidEN_US = validator.isAlpha(name, ['en-US'], {
+      ignore: ' \''
+    })
+
+    return itsValidPT_BR || itsValidEN_US
+  }
+
   async create(req, res) {
     try {
+      let errorFields = []
 
+      /* ##### CAMPOS OBRIGATÓRIOS ##### */
+
+      if (req.body.name) {
+        let name = req.body.name
+        if (!this.isValidName(name)) {
+          errorFields.push({
+            field: 'iptName',
+            error: 'O nome informado é inválido.'
+          })
+        }
+      } else {
+        errorFields.push({
+          field: 'iptName',
+          error: 'Este campo é obrigatório.'
+        })
+      }
     } catch (error) {
       res.status(500)
       throw new Error(error)
