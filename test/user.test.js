c2afb0aa3875b4ba557de62dc17e22d420f3e382
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-06 19:00:50 -0400

    Implementado codigo para teste do campo de Email do User.

--- test/user.test.js
+++ test/user.test.js
@@ -7,13 +7,17 @@ const UserController = require('../src/controllers/UserController')
 const request = supertest(app)
 
 describe("Suite de testes das rotas User.", function() {
-  test("Devem retorar TRUE para os valores informados.", async function() {
+  test("Devem retornar TRUE para os valores informados.", async function() {
     try {
       const userController = UserController
 
       // Teste para o campo Nome
       let name = userController.isValidName('Tobias de Oliveira')
       expect(name).toEqual(true)
+
+      // Teste para o campo Email
+      let email = userController.isValidEmail('tobias@gmail.com')
+      expect(email).toEqual(true)
     } catch (error) {
       fail(error)
     }
