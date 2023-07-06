cd4698da6885ca6844c72e4da71985bf59884742
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-06 15:24:10 -0400

    Criada rota POST para cadastro de um Usuario.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,5 +7,6 @@ const HomeController = require('../controllers/HomeController')
 const UserController = require('../controllers/UserController')
 
 router.get('/', HomeController.index)
+router.post('/users', UserController.create)
 
 module.exports = router
