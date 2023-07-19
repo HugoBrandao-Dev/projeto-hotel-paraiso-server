b2a6a5368eaba9b61af062781e2210c59690ce9c
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-18 22:33:32 -0400

    Implementado metodo que salva as informacoes de um novo Usuario.

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,16 @@
 let UserCollection = require('../data/UserCollection.json')
 
 class User {
+  async save(user) {
+    try {
+      await UserCollection.users.data.push(user)
+      return
+    } catch (error) {
+      console.log(error)
+      return
+    }
+  }
+
   async findOne(id) {
     try {
       const user = await UserCollection.users.data.find(doc => doc.id == id )
