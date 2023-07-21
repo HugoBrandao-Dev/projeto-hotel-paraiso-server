a6802ae564de9ab3a6cc19d48ccf8404a0a9c98e
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-20 21:35:46 -0400

    Criado endpoint para atualizacao das informacoes de um usuario.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,10 +7,19 @@ const HomeController = require('../controllers/HomeController')
 const UserController = require('../controllers/UserController')
 
 router.get('/', HomeController.index)
+
+/* ########### USER ########### */
+
+// Create
 router.post('/users', UserController.create)
+
+// Read
 router.post('/users/by_cpf', UserController.readByCPF)
 router.post('/users/by_passportNumber', UserController.readByPassportNumber)
 router.get('/users', UserController.readMany)
 router.get('/users/:id', UserController.read)
+
+// Update
+router.put('/users', UserController.update)
 
 module.exports = router
