eec310d13b399afdd8410d797662e3fa46b829f4
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-22 16:43:19 -0400

    Criado teste para FALHA no cadastro de um usuario que nao informada seu email.

--- test/user.test.js
+++ test/user.test.js
@@ -158,6 +158,33 @@ describe("Suite de testes das rotas User.", function() {
           fail(error)
         })
     })
+
+    test("POST - Deve retornar 400, pela ausencia do nome do User.", function() {
+      return request.post('/users').send({
+        name: "Tobias de Oliveira",
+        email: "",
+        password: "@TobiaS&591022@",
+        phoneCode: "55",
+        phoneNumber: "11984752352",
+        birthDate: "1985-06-09",
+        country: "BR",
+        state: "SP",
+        city: "São Paulo",
+        cpf: "22222222222",
+      })
+        .then(function(response) {
+          expect(response.statusCode).toEqual(400)
+          expect(response.body.RestException.Code).toBe("1")
+          expect(response.body.RestException.Message).toBe("O campo Email é obrigatório.")
+          expect(response.body.RestException.Status).toBe("400")
+          expect(response.body.RestException.MoreInfo).toBe("/docs/erros/1")
+          expect(response.body.RestException.ErrorFields[0].field).toBe('iptEmail')
+          expect(response.body.RestException.ErrorFields[0].hasError.error).toBe('O campo Email é obrigatório.')
+        })
+        .catch(function(error) {
+          fail(error)
+        })
+    })
   })
 
   /* ################## READ ################## */
