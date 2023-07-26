f9c5b199a65e0dc0683afbc4fc96c377fbbeead2
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-26 16:58:08 -0400

    Criado teste para FALHA no cadastro de um usuario Brasileiro que tenha informado um CPF que ja foi cadastrado.

--- test/user.test.js
+++ test/user.test.js
@@ -725,6 +725,33 @@ describe("Suite de testes das rotas User.", function() {
           fail(error)
         })
     })
+
+    test("POST - Deve retornar 400, devido ao CPF informado já estar sido cadastrado.", function() {
+      return request.post('/users').send({
+        name: "Tobias de Oliveira",
+        email: "tobias@gmail.com",
+        password: "@TobiaS&6554987@",
+        phoneCode: "55",
+        phoneNumber: "115498653214",
+        birthDate: "2000-02-11",
+        country: "BR",
+        state: "SP",
+        city: "São Manuel",
+        cpf: "33333333333",
+      })
+        .then(function(response) {
+          expect(response.statusCode).toEqual(400)
+          expect(response.body.RestException.Code).toBe("1")
+          expect(response.body.RestException.Message).toBe("O CPF informado já está cadastrado")
+          expect(response.body.RestException.Status).toBe("400")
+          expect(response.body.RestException.MoreInfo).toBe("/docs/erros/1")
+          expect(response.body.RestException.ErrorFields[0].field).toBe('iptCPF')
+          expect(response.body.RestException.ErrorFields[0].hasError.error).toBe("O CPF informado já está cadastrado")
+        })
+        .catch(function(error) {
+          fail(error)
+        })
+    })
   })
 
   /* ################## READ ################## */
