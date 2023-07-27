9767ca55f8846271d6c1eea9f4eef8df2ed227fd
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-27 18:55:22 -0400

    Criado teste para FALHA no cadastro de um usuario Brasileiro que tenha informado um CEP contendo caracteres invalidos.

--- test/user.test.js
+++ test/user.test.js
@@ -889,6 +889,34 @@ describe("Suite de testes das rotas User.", function() {
           fail(error)
         })
     })
+
+    test("POST - Deve retornar 400, devido ao número de CEP possuir caractere inválido.", function() {
+      return request.post('/users').send({
+        name: "Tobias de Oliveira",
+        email: "tobias@gmail.com",
+        password: "@TobiaS&6554987@",
+        phoneCode: "55",
+        phoneNumber: "115498653214",
+        birthDate: "2000-02-11",
+        cpf: "22222222222",
+        country: "BR",
+        state: "SP",
+        city: "São Manuel",
+        cep: "1010010a"
+      })
+        .then(function(response) {
+          expect(response.statusCode).toEqual(400)
+          expect(response.body.RestException.Code).toBe("1")
+          expect(response.body.RestException.Message).toBe("O campo de CEP possui caracteres inválidos")
+          expect(response.body.RestException.Status).toBe("400")
+          expect(response.body.RestException.MoreInfo).toBe("/docs/erros/1")
+          expect(response.body.RestException.ErrorFields[0].field).toBe('iptCEP')
+          expect(response.body.RestException.ErrorFields[0].hasError.error).toBe("O campo de CEP possui caracteres inválidos")
+        })
+        .catch(function(error) {
+          fail(error)
+        })
+    })
   })
 
   /* ################## READ ################## */
