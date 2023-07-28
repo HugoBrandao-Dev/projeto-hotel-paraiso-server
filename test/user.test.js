06f2b182d0168fe2f46e1f2574f1cca189a42c43
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-28 17:18:45 -0400

    Criado teste para FALHA na leitura de um usuario com ID que nao existe.

--- test/user.test.js
+++ test/user.test.js
@@ -1459,30 +1459,24 @@ describe("Suite de testes das rotas User.", function() {
     test("GET - Deve retornar 400, devido ao número de ID conter caractere inválido.", function() {
       return request.get('/users/5da9ea674234*635bdff45c02')
         .then(response => {
-          let { 
-            name,
-            email,
-            birthDate,
-            phoneCode,
-            phoneNumber,
-            country,
-            cep,
-            state,
-            city,
-            cpf,
-            passportNumber,
-            neighborhood,
-            road,
-            house_number,
-            information,
-            created,
-            updated
-          } = response.body
-
           expect(response.statusCode).toEqual(400)
           expect(response.body.RestException.Code).toBe("2")
           expect(response.body.RestException.Message).toBe("O parâmetro ID possui caracteres inválidos")
           expect(response.body.RestException.Status).toBe("400")
+          expect(response.body.RestException.MoreInfo).toBe("/docs/erros/2")
+        })
+        .catch(error => {
+          fail(error)
+        })
+    })
+
+    test("GET - Deve retornar 400, devido ao número de ID conter caractere inválido.", function() {
+      return request.get('/users/5da9ea674234635bdff00000')
+        .then(response => {
+          expect(response.statusCode).toEqual(404)
+          expect(response.body.RestException.Code).toBe("2")
+          expect(response.body.RestException.Message).toBe("Nenhum usuário com o ID informado está cadastrado")
+          expect(response.body.RestException.Status).toBe("404")
           expect(response.body.RestException.MoreInfo).toBe("/docs/erros/2")
         })
         .catch(error => {
