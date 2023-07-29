cc676349694dbf392e337e9ffc4d2bfcd4fe1134
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-28 21:42:47 -0400

    Criado teste para FALHA no cadastro de um usuario que possui um valor invalido de funcao(role).

--- test/user.test.js
+++ test/user.test.js
@@ -380,6 +380,35 @@ describe("Suite de testes das rotas User.", function() {
         })
     })
 
+    // Testes na FUNÇÃO DA CONTA QUE ESTÁ SENDO CRIADA (role)
+    test("POST - Deve retornar 400, devido a função informada ter caracteres inválidos.", function() {
+      return request.post('/users').send({
+        name: "Tobias de Oliveira",
+        email: "tobias@gmail.com",
+        password: "@TobiaS&7qer5464@",
+        role: "cliente", // role deve ser um valor numérico
+        phoneCode: "55",
+        phoneNumber: "11984752352",
+        birthDate: "2000-02-11",
+        country: "BR",
+        state: "SP",
+        city: "São Paulo",
+        cpf: "22222222222",
+      })
+        .then(function(response) {
+          expect(response.statusCode).toEqual(400)
+          expect(response.body.RestException.Code).toBe("1")
+          expect(response.body.RestException.Message).toBe("O campo de Role possui caracteres inválidos")
+          expect(response.body.RestException.Status).toBe("400")
+          expect(response.body.RestException.MoreInfo).toBe("/docs/erros/1")
+          expect(response.body.RestException.ErrorFields[0].field).toBe('iptRole')
+          expect(response.body.RestException.ErrorFields[0].hasError.error).toBe("O campo de Role possui caracteres inválidos")
+        })
+        .catch(function(error) {
+          fail(error)
+        })
+    })
+
     // Testes no CÓDIGO DO TELEFONE/TELEFONE
     test("POST - Deve retornar 400, pela ausência do código do telefone.", function() {
       return request.post('/users').send({
