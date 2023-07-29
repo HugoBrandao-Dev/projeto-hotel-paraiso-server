254ddf370e9230805e056e873d5ed79e96f35bbc
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-07-28 22:02:17 -0400

    Criado teste para FALHA no cadastro de um usuario que possui um valor fora do intervalo valido para funcoes (roles) disponiveis.

--- test/user.test.js
+++ test/user.test.js
@@ -381,7 +381,7 @@ describe("Suite de testes das rotas User.", function() {
     })
 
     // Testes na FUNÇÃO DA CONTA QUE ESTÁ SENDO CRIADA (role)
-    test("POST - Deve retornar 400, devido a função informada ter caracteres inválidos.", function() {
+    test("POST - Deve retornar 400, devido a função (role) informada ter caracteres inválidos.", function() {
       return request.post('/users').send({
         name: "Tobias de Oliveira",
         email: "tobias@gmail.com",
@@ -403,6 +403,34 @@ describe("Suite de testes das rotas User.", function() {
           expect(response.body.RestException.MoreInfo).toBe("/docs/erros/1")
           expect(response.body.RestException.ErrorFields[0].field).toBe('iptRole')
           expect(response.body.RestException.ErrorFields[0].hasError.error).toBe("O campo de Role possui caracteres inválidos")
+        })
+        .catch(function(error) {
+          fail(error)
+        })
+    })
+
+    test("POST - Deve retornar 400, devido ao valor da função (role) ser de uma função inexistente.", function() {
+      return request.post('/users').send({
+        name: "Tobias de Oliveira",
+        email: "tobias@gmail.com",
+        password: "@TobiaS&7qer5464@",
+        role: "10",
+        phoneCode: "55",
+        phoneNumber: "11984752352",
+        birthDate: "2000-02-11",
+        country: "BR",
+        state: "SP",
+        city: "São Paulo",
+        cpf: "22222222222",
+      })
+        .then(function(response) {
+          expect(response.statusCode).toEqual(400)
+          expect(response.body.RestException.Code).toBe("1")
+          expect(response.body.RestException.Message).toBe("O valor de Role é de uma função inexistente")
+          expect(response.body.RestException.Status).toBe("400")
+          expect(response.body.RestException.MoreInfo).toBe("/docs/erros/1")
+          expect(response.body.RestException.ErrorFields[0].field).toBe('iptRole')
+          expect(response.body.RestException.ErrorFields[0].hasError.error).toBe("O valor de Role é de uma função inexistente")
         })
         .catch(function(error) {
           fail(error)
