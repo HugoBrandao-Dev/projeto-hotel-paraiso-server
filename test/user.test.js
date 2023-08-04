78c748cd935a92cdf8da47191dbce4ded61e4da9
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-04 11:33:09 -0400

    Criado teste de SUCESSO na atualizacao somente do nome, senha e role (funcao) do usuario.

--- test/user.test.js
+++ test/user.test.js
@@ -1940,6 +1940,38 @@ describe("Suite de testes das rotas User.", function() {
         })
     })
 
+    test("POST - Deve retornar 200 para usuários Brasileiro que querem atualizar somente o Nome, Senha e Função.", function() {
+      let user = {
+        id: "5da9ea674234635bdff45c02",
+        name: "Josias de Oliveira",
+        password: "@JosiaS&654975@",
+        role: '1'
+      }
+      return request.put('/users').send(user)
+        .then(function(responsePOST) {
+          expect(responsePOST.statusCode).toEqual(200)
+          return request.get(`/users/${ user.id }`)
+            .then(function(responseGET) {
+              expect(responseGET.statusCode).toEqual(200)
+
+              expect(responseGET.body.name).toBeDefined()
+              expect(responseGET.body.name).toBe(user.name)
+
+              expect(responseGET.body.password).toBeDefined()
+              expect(responseGET.body.password).toBe(user.password)
+
+              expect(responseGET.body.role).toBeDefined()
+              expect(responseGET.body.role).toBe(user.role)
+            })
+            .catch(function(errorGET) {
+              fail(errorGET)
+            })
+        })
+        .catch(function(errorPOST) {
+          fail(errorPOST)
+        })
+    })
+
       /*
     test("POST - Deve retornar 200 e o usuário estrangeiro com suas informações obrigatórias atualizadas.", function() {
       let user = {
