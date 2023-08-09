eea04b99ec00b959c531837ce781b6e25ae1aaad
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-08 22:20:11 -0400

    Criado teste de SUCESSO para usuario Brasileiro que muda seu documento de CPF para Numero de Passaporte.

--- test/user.test.js
+++ test/user.test.js
@@ -2055,6 +2055,74 @@ describe("Suite de testes das rotas User.", function() {
         })
     })
 
+    test("POST - Deve retornar 200 para o usuário Brasileiro que modifica seu local de nascimento e informa o Número do Passaporte.", function() {
+      const fixedPassportNumber = genPassportNumber()
+      let user = {
+        id: "5da9ea674234635bdff45c02",
+        name: "Jeremias de Oliveira",
+        email: "jere_oli@yahoo.com",
+        country: "US",
+        state: "MN",
+        city: "Saint Paul",
+        passportNumber: fixedPassportNumber,
+      }
+      return request.put('/users').send(user)
+        .then(function(responsePUT) {
+          expect(responsePUT.statusCode).toEqual(200)
+          return request.get(`/users/${ user.id }`)
+            .then(function(responseGET) {
+              const {
+                id,
+                name,
+                email,
+                country,
+                state,
+                city,
+                cpf,
+                passportNumber
+              } = responseGET.body
+
+              // ID
+              expect(id).toBeDefined()
+              expect(id).toBe(user.id)
+
+              // Nome
+              expect(name).toBeDefined()
+              expect(name).toBe(user.name)
+
+              // Email
+              expect(email).toBeDefined()
+              expect(email).toBe(user.email)
+
+              // País de nascimento
+              expect(country).toBeDefined()
+              expect(country).toBe(user.country)
+
+              // Estado de nascimento
+              expect(state).toBeDefined()
+              expect(state).toBe(user.state)
+
+              // Cidade de nascimento
+              expect(city).toBeDefined()
+              expect(city).toBe(user.city)
+
+              // CPF
+              expect(cpf).toBeDefined()
+              expect(cpf).toBe('')
+
+              // Número do passaporte
+              expect(passportNumber).toBeDefined()
+              expect(passportNumber).toBe(user.passportNumber)
+            })
+            .catch(function(errorGET) {
+              fail(errorGET)
+            })
+        })
+        .catch(function(errorPUT) {
+          fail(errorPUT)
+        })
+    })
+
     test("POST - Deve retornar 200 para usuários que informa o mesmo Número de Passaporte.", function() {
       let user = {
         id: "507f191e810c19729de860ea",
