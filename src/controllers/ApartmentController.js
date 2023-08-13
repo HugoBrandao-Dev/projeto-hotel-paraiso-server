ef33d20cc3da2d776a0387067ac290187fb30f56
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-12 23:39:09 -0400

    Implementado codigo de chamada do metodo de analise do valor do Piso do apartamento, no metodo de criacao de um novo apartamento.

--- src/controllers/ApartmentController.js
+++ src/controllers/ApartmentController.js
@@ -9,7 +9,48 @@ const projectLinks = {
 class ApartmentController {
   async create(req, res, next) {
     try {
+      let errorFields = []
+      const {
+        floor,
+        number,
+        status,
+        user_id,
+        start,
+        end
+      } = req.body
 
+      const floorResult = Analyzer.analyzeApartmentFloor(floor)
+      if (floorResult.hasError.value) {
+        errorFields.push(floorResult)
+      }
+
+      if (errorFields.length) {
+        let codes = errorFields.map(item => item.hasError.type)
+
+        // Cria um array contendo os Status codes dos erros encontrados.
+        let status = codes.map(code => {
+          switch(code) {
+            case 3:
+              return '404'
+              break
+            default:
+              return '400'
+          }
+        })
+        let messages = errorFields.map(item => item.hasError.error)
+        let moreinfos = errorFields.map(item => `${ projectLinks.errors }/${ item.hasError.type }`)
+        res.status(400)
+        res.json({ 
+          RestException: {
+            "Code": codes.length > 1 ? codes.join(';') : codes.toString(),
+            "Message": messages.length > 1 ? messages.join(';') : messages.toString(),
+            "Status": status.length > 1 ? status.join(';') : status.toString(),
+            "MoreInfo": moreinfos.length > 1 ? moreinfos.join(';') : moreinfos.toString(),
+            "ErrorFields": errorFields
+          }
+        })
+        return
+      }
     } catch(error) {
       next(error)
     }
