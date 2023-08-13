28f8089ef660eb1ab928b3103b4610dca732786c
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-12 23:10:14 -0400

    Implementados codigos de configuracoes basicas para o controller de Apartment.

---
+++ src/controllers/ApartmentController.js
@@ -0,0 +1,51 @@
+const Analyzer = require('../tools/Analyzer')
+const uuid = require('uuid')
+
+let baseURL = 'http://localhost:4000'
+const projectLinks = {
+  errors: 'https://projetohotelparaiso.dev/docs/erros'
+}
+
+class ApartmentController {
+  async create(req, res, next) {
+    try {
+
+    } catch(error) {
+      next(error)
+    }
+  }
+
+  async read(req, res, next) {
+    try {
+
+    } catch(error) {
+      next(error)
+    }
+  }
+
+  async list(req, res, next) {
+    try {
+
+    } catch(error) {
+      next(error)
+    }
+  }
+
+  async update(req, res, next) {
+    try {
+
+    } catch(error) {
+      next(error)
+    }
+  }
+
+  async delete(req, res, next) {
+    try {
+
+    } catch(error) {
+      next(error)
+    }
+  }
+}
+
+module.exports = new ApartmentController()
