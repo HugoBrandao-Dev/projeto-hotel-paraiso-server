e6e0ee5133dc061287f0c4103f1a6f02613234ed
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-14 16:57:40 -0400

    Definida estrutura basica do model de Apartment.

---
+++ src/models/Apartment.js
@@ -0,0 +1,41 @@
+let ApartmentCollection = require('../data/ApartmentCollection.json')
+let DateFormated = require('../tools/DateFormated')
+let uuid = require('uuid')
+
+class Apartment {
+  async save(apartment) {
+    try {
+
+    } catch (error) {
+      console.log(error)
+      return
+    }
+  }
+
+  async findOne(id) {
+    try {
+
+    } catch (error) {
+      console.log(error)
+      return []
+    }
+  }
+
+  async edit(apartment) {
+    try {
+
+    } catch (error) {
+      console.log(error)
+      return
+    }
+  }
+
+  async delete(id) {
+    try {
+
+    } catch (error) {
+      console.log(error)
+      return
+    }
+  }
+}
