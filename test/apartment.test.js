b27691465f76678a9c59d15955566db52a005206
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-16 12:13:07 -0400

    Criado teste de FALHA no cadastro de um apartamento, devido a presenca de caracteres invalidos na quantidade de um comodo.

--- test/apartment.test.js
+++ test/apartment.test.js
@@ -273,7 +273,7 @@ describe("Suite de testes das rotas de Apartment.", function() {
           })
       })
 
-      test("/POST - Deve retornar 400, pela presença de campos inválidos nos cômodos do apartamento.", function() {
+      test("/POST - Deve retornar 400, pela ausência do valor do nome do campo obrigatório.", function() {
         let apartment = {
           floor: "3",
           number: "10",
@@ -381,7 +381,32 @@ describe("Suite de testes das rotas de Apartment.", function() {
           })
       })
 
+      test("/POST - Deve retornar 400, pela ausência de um campo no cômodo do apartamento.", function() {
+        let apartment = {
+          floor: "3",
+          number: "10",
+          rooms: [
+            {
+              room: 'sala de estar',
+              quantity: 'um'
+            }
+          ]
+        }
+        return request.post('/apartments').send(apartment)
+          .then(function(response) {
+            expect(response.statusCode).toEqual(400)
 
+            expect(response.body.RestException.Code).toBe("2")
+            expect(response.body.RestException.Message).toBe(`A quantidade de ${ apartment.rooms[0].room } possui caracteres inválidos`)
+            expect(response.body.RestException.Status).toBe("400")
+            expect(response.body.RestException.MoreInfo).toBe(`${ projectLinks.erros }/2`)
+            expect(response.body.RestException.ErrorFields[0].field).toBe('iptRooms')
+            expect(response.body.RestException.ErrorFields[0].hasError.error).toBe(`A quantidade de ${ apartment.rooms[0].room } possui caracteres inválidos`)
+          })
+          .catch(function(error) {
+            fail(error)
+          })
+      })
     })
   })
   describe("READ", function() {
