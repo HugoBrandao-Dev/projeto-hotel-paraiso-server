4f6a71cf57054b94fb52d78bcf266f2c6a909486
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-16 16:42:06 -0400

    Criado teste de FALHA no cadastro de um apartamento, devido a presenca de caracteres invalidos no preco da diaria.

--- test/apartment.test.js
+++ test/apartment.test.js
@@ -515,6 +515,46 @@ describe("Suite de testes das rotas de Apartment.", function() {
             fail(error)
           })
       })
+
+      test("/POST - Deve retornar 400, devido a presença de caracteres inválidos no valor da diária.", function() {
+        let apartment = {
+          floor: "3",
+          number: "10",
+          rooms: [
+            {
+              room: 'sala de estar',
+              quantity: '1'
+            },
+            {
+              room: 'cozinha',
+              quantity: '1'
+            },
+            {
+              room: 'banheiro',
+              quantity: '1'
+            },
+            {
+              room: 'quarto',
+              quantity: '1'
+            }
+          ],
+          daily_price: '800a'
+        }
+        return request.post('/apartments').send(apartment)
+          .then(function(response) {
+            expect(response.statusCode).toEqual(400)
+
+            expect(response.body.RestException.Code).toBe("2")
+            expect(response.body.RestException.Message).toBe("O valor da diária contém caracteres inválidos")
+            expect(response.body.RestException.Status).toBe("400")
+            expect(response.body.RestException.MoreInfo).toBe(`${ projectLinks.erros }/2`)
+            expect(response.body.RestException.ErrorFields[0].field).toBe('iptDailyPrice')
+            expect(response.body.RestException.ErrorFields[0].hasError.error).toBe("O valor da diária contém caracteres inválidos")
+          })
+          .catch(function(error) {
+            fail(error)
+          })
+      })
     })
   })
   describe("READ", function() {
