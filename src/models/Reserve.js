281c658169c5e7473a37be095949a021794c7b24
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-22 13:30:56 -0400

    Implentado codigo de configuracao basica para o model de Reservas.

---
+++ src/models/Reserve.js
@@ -0,0 +1,12 @@
+let ApartmentCollection = require('../data/ApartmentCollection.json')
+let DateFormated = require('../tools/DateFormated')
+
+const date = new DateFormated('mongodb')
+
+class Reserve {
+  async edit(reserve) {
+
+  }
+}
+
+module.exports = new Reserve()
