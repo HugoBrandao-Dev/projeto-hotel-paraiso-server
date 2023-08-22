644a8952dec5af06978f6cc4a175abef83aa5405
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-22 14:17:17 -0400

    Implementado codigo de chamada do metodo de analise do ID, para o campo de ID do apartamento, no metodo de atualizacao das informacoes de uma reserva.

--- src/controllers/ReserveController.js
+++ src/controllers/ReserveController.js
@@ -19,7 +19,51 @@ class ReserveController {
 
   async update(req, res, next) {
     try {
+      const {
+        apartment_id,
+        status,
+        user_id,
+        date,
+        start,
+        end
+      } = req.body
 
+      let errorFields = []
+
+      let apartmentIDResult = await Analyzer.analyzeID(apartment_id, 'apartment')
+      if (apartmentIDResult.hasError.value) {
+        errorFields.push(apartmentIDResult)
+      }
+
+      if (errorFields.length) {
+        let codes = errorFields.map(item => item.hasError.type)
+
+        // Cria um array contendo os Status codes dos erros encontrados.
+        let status = codes.map(code => {
+          switch(code) {
+            case 3:
+              return '404'
+              break
+            default:
+              return '400'
+          }
+        })
+        let messages = errorFields.map(item => item.hasError.error)
+        let moreinfos = errorFields.map(item => `${ projectLinks.errors }/${ item.hasError.type }`)
+        res.status(400)
+        res.json({ 
+          RestException: {
+            "Code": codes.length > 1 ? codes.join(';') : codes.toString(),
+            "Message": messages.length > 1 ? messages.join(';') : messages.toString(),
+            "Status": status.length > 1 ? status.join(';') : status.toString(),
+            "MoreInfo": moreinfos.length > 1 ? moreinfos.join(';') : moreinfos.toString(),
+            "ErrorFields": errorFields
+          }
+        })
+        return
+      }
+
+      res.sendStatus(200)
     } catch (error) {
       next(error)
     }
