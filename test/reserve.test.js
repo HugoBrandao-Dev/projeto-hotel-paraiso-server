f736fda376d2afee1f4afca879f9509c1a8e6bc5
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-22 14:15:04 -0400

    Criado teste de FALHA na atualizacao das informacoes da Reserva, devido a ausencia do valor do ID do apartamento.

--- test/reserve.test.js
+++ test/reserve.test.js
@@ -11,6 +11,31 @@ const projectLinks = {
 describe("Suite de teste para as Reservas.", function() {
   describe("UPDATE", function() {
     describe("Testes de SUCESSO.", function() {})
-    describe("Testes de FALHA.", function() {})
+    describe("Testes de FALHA.", function() {
+      test("/PUT - Deve retornar 400, por não conter o ID do apartamento a ser reservado.", function() {
+        let reserve = {
+          apartment_id: "",
+          status: "livre",
+          user_id: "507f1f77bcf86cd799439011",
+          date: "2023-08-12T22:49:04.421Z",
+          start: "2023-11-12",
+          end: "2024-01-12"
+        }
+        return request.put('/reserves').send(reserve)
+          .then(function(response) {
+            expect(response.statusCode).toEqual(400)
+
+            expect(response.body.RestException.Code).toBe("1")
+            expect(response.body.RestException.Message).toBe("O ID do apartamento é obrigatório")
+            expect(response.body.RestException.Status).toBe("400")
+            expect(response.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/1`)
+            expect(response.body.RestException.ErrorFields[0].field).toBe('id')
+            expect(response.body.RestException.ErrorFields[0].hasError.error).toBe('O ID do apartamento é obrigatório')
+          })
+          .catch(function(error) {
+            fail(error)
+          })
+      })
+    })
   })
 })
