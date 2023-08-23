ca461fd6b0aad40c01a927a94a6a83c7ee2f2bd6
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-23 14:04:16 -0400

    Criado teste de FALHA na atualizacao das informacoes da Reserva, devido a ausencia do campo de status da reserva.

--- test/reserve.test.js
+++ test/reserve.test.js
@@ -61,6 +61,31 @@ describe("Suite de teste para as Reservas.", function() {
             fail(error)
           })
       })
+
+      test("/PUT - Deve retornar 400, uma vez que NÃO foi informado o Status.", function() {
+        let reserve = {
+          apartment_id: "856377c88f8fd9fc65fd3ef5",
+          status: "",
+          user_id: "507f1f77bcf86cd799439011",
+          date: "2023-08-12T22:49:04.421Z",
+          start: "2023-11-12",
+          end: "2024-01-12"
+        }
+        return request.put('/reserves').send(reserve)
+          .then(function(response) {
+            expect(response.statusCode).toEqual(400)
+
+            expect(response.body.RestException.Code).toBe("1")
+            expect(response.body.RestException.Message).toBe("O campo de Status é obrigatório")
+            expect(response.body.RestException.Status).toBe("400")
+            expect(response.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/1`)
+            expect(response.body.RestException.ErrorFields[0].field).toBe('iptStatus')
+            expect(response.body.RestException.ErrorFields[0].hasError.error).toBe('O campo de Status é obrigatório')
+          })
+          .catch(function(error) {
+            fail(error)
+          })
+      })
     })
   })
 })
