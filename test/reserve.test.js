fefb61598837952283d0ecb6fecbb4e9b26114c7
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-23 16:24:55 -0400

    Criado teste de FALHA na atualizacao das informacoes da Reserva, por tentar reservar um apartamento que ja esta reservado, ocupado ou indiponivel.

--- test/reserve.test.js
+++ test/reserve.test.js
@@ -111,6 +111,31 @@ describe("Suite de teste para as Reservas.", function() {
             fail(error)
           })
       })
+
+      test("/PUT - Deve retornar 400, já que o apartamento já está reservado, ocupado ou indisponível.", function() {
+        let reserve = {
+          apartment_id: "856377c88f8fd9fc65fd3ef5",
+          status: "reservado",
+          user_id: "507f1f77bcf86cd799439011",
+          date: "2023-08-12T22:49:04.421Z",
+          start: "2023-11-12",
+          end: "2024-01-12"
+        }
+        return request.put('/reserves').send(reserve)
+          .then(function(response) {
+            expect(response.statusCode).toEqual(400)
+
+            expect(response.body.RestException.Code).toBe("4")
+            expect(response.body.RestException.Message).toBe("O apartamento escolhido já está Reservado, Ocupado ou Indisponível")
+            expect(response.body.RestException.Status).toBe("400")
+            expect(response.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/4`)
+            expect(response.body.RestException.ErrorFields[0].field).toBe('iptStatus')
+            expect(response.body.RestException.ErrorFields[0].hasError.error).toBe("O apartamento escolhido já está Reservado, Ocupado ou Indisponível")
+          })
+          .catch(function(error) {
+            fail(error)
+          })
+      })
     })
   })
 })
