ec0593113d391a262e15d0e5c91c2be6e1d0a3cb
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-23 17:58:15 -0400

    Criado teste de FALHA na atualizacao das informacoes da Reserva, por ter informado o ID do cliente com caracteres invalidos.

--- test/reserve.test.js
+++ test/reserve.test.js
@@ -161,6 +161,31 @@ describe("Suite de teste para as Reservas.", function() {
             fail(error)
           })
       })
+
+      test("/PUT - Deve retornar 400, já que ID do cliente contém caractere inválido.", function() {
+        let reserve = {
+          apartment_id: "02n07j2d1hf5a2f26djjj92a",
+          status: "reservado",
+          user_id: "507f1f77bcf86cd79943901*",
+          date: "2023-08-12T22:49:04.421Z",
+          start: "2023-11-12",
+          end: "2024-01-12"
+        }
+        return request.put('/reserves').send(reserve)
+          .then(function(response) {
+            expect(response.statusCode).toEqual(400)
+
+            expect(response.body.RestException.Code).toBe("2")
+            expect(response.body.RestException.Message).toBe("O ID do cliente/usuário contém caracteres inválidos")
+            expect(response.body.RestException.Status).toBe("400")
+            expect(response.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/2`)
+            expect(response.body.RestException.ErrorFields[0].field).toBe('iptClient')
+            expect(response.body.RestException.ErrorFields[0].hasError.error).toBe("O ID do cliente/usuário contém caracteres inválidos")
+          })
+          .catch(function(error) {
+            fail(error)
+          })
+      })
     })
   })
 })
