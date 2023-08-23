c66cdc6409146c67ba5702c67e7a633733ac75e9
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-23 14:57:22 -0400

    Criado teste de FALHA na atualizacao das informacoes da Reserva, devido ao valor do campo de Status ser invalido.

--- test/reserve.test.js
+++ test/reserve.test.js
@@ -86,6 +86,31 @@ describe("Suite de teste para as Reservas.", function() {
             fail(error)
           })
       })
+
+      test("/PUT - Deve retornar 400, já que o valor de Status é inválido.", function() {
+        let reserve = {
+          apartment_id: "856377c88f8fd9fc65fd3ef5",
+          status: "1",
+          user_id: "507f1f77bcf86cd799439011",
+          date: "2023-08-12T22:49:04.421Z",
+          start: "2023-11-12",
+          end: "2024-01-12"
+        }
+        return request.put('/reserves').send(reserve)
+          .then(function(response) {
+            expect(response.statusCode).toEqual(400)
+
+            expect(response.body.RestException.Code).toBe("2")
+            expect(response.body.RestException.Message).toBe("O valor do campo de Status é inválido")
+            expect(response.body.RestException.Status).toBe("400")
+            expect(response.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/2`)
+            expect(response.body.RestException.ErrorFields[0].field).toBe('iptStatus')
+            expect(response.body.RestException.ErrorFields[0].hasError.error).toBe('O valor do campo de Status é inválido')
+          })
+          .catch(function(error) {
+            fail(error)
+          })
+      })
     })
   })
 })
