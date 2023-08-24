a2047ccfc601c6837bb8145d5f0d5f233b8d66a3
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-24 12:40:31 -0400

    Criado teste de FALHA na atualizacao das informacoes da Reserva, por ter informado uma Data de Inicio contendo caracteres invalidos.

--- test/reserve.test.js
+++ test/reserve.test.js
@@ -226,6 +226,30 @@ describe("Suite de teste para as Reservas.", function() {
             fail(error)
           })
       })
+
+      test("/PUT - Deve retornar 400, devido a ausência da Data de Início da reserva.", function() {
+        let reserve = {
+          apartment_id: "02n07j2d1hf5a2f26djjj92a",
+          status: "reservado",
+          user_id: "600f191e810c19829de900ea",
+          start: "2023-12-*2",
+          end: "2024-01-12"
+        }
+        return request.put('/reserves').send(reserve)
+          .then(function(response) {
+            expect(response.statusCode).toEqual(400)
+
+            expect(response.body.RestException.Code).toBe("2")
+            expect(response.body.RestException.Message).toBe("O campo de Data de Início da reserva possui caracteres inválidos")
+            expect(response.body.RestException.Status).toBe("400")
+            expect(response.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/2`)
+            expect(response.body.RestException.ErrorFields[0].field).toBe('iptStartDate')
+            expect(response.body.RestException.ErrorFields[0].hasError.error).toBe("O campo de Data de Início da reserva possui caracteres inválidos")
+          })
+          .catch(function(error) {
+            fail(error)
+          })
+      })
     })
   })
 })
