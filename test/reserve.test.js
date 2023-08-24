f7f7773935de92be37a69a0e9e2bdc87c3f526fa
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-24 12:23:29 -0400

    Criado teste de FALHA na atualizacao das informacoes da Reserva, por nao ter informado uma Data de Inicio da reserva.

--- test/reserve.test.js
+++ test/reserve.test.js
@@ -17,7 +17,6 @@ describe("Suite de teste para as Reservas.", function() {
           apartment_id: "",
           status: "livre",
           user_id: "507f1f77bcf86cd799439011",
-          date: "2023-08-12T22:49:04.421Z",
           start: "2023-11-12",
           end: "2024-01-12"
         }
@@ -42,7 +41,6 @@ describe("Suite de teste para as Reservas.", function() {
           apartment_id: "sdf*q98-we7",
           status: "livre",
           user_id: "507f1f77bcf86cd799439011",
-          date: "2023-08-12T22:49:04.421Z",
           start: "2023-11-12",
           end: "2024-01-12"
         }
@@ -67,7 +65,6 @@ describe("Suite de teste para as Reservas.", function() {
           apartment_id: "856377c88f8fd9fc65fd3ef5",
           status: "",
           user_id: "507f1f77bcf86cd799439011",
-          date: "2023-08-12T22:49:04.421Z",
           start: "2023-11-12",
           end: "2024-01-12"
         }
@@ -92,7 +89,6 @@ describe("Suite de teste para as Reservas.", function() {
           apartment_id: "856377c88f8fd9fc65fd3ef5",
           status: "1",
           user_id: "507f1f77bcf86cd799439011",
-          date: "2023-08-12T22:49:04.421Z",
           start: "2023-11-12",
           end: "2024-01-12"
         }
@@ -117,7 +113,6 @@ describe("Suite de teste para as Reservas.", function() {
           apartment_id: "856377c88f8fd9fc65fd3ef5",
           status: "reservado",
           user_id: "507f1f77bcf86cd799439011",
-          date: "2023-08-12T22:49:04.421Z",
           start: "2023-11-12",
           end: "2024-01-12"
         }
@@ -142,7 +137,6 @@ describe("Suite de teste para as Reservas.", function() {
           apartment_id: "02n07j2d1hf5a2f26djjj92a",
           status: "reservado",
           user_id: "",
-          date: "2023-08-12T22:49:04.421Z",
           start: "2023-11-12",
           end: "2024-01-12"
         }
@@ -167,7 +161,6 @@ describe("Suite de teste para as Reservas.", function() {
           apartment_id: "02n07j2d1hf5a2f26djjj92a",
           status: "reservado",
           user_id: "507f1f77bcf86cd79943901*",
-          date: "2023-08-12T22:49:04.421Z",
           start: "2023-11-12",
           end: "2024-01-12"
         }
@@ -192,7 +185,6 @@ describe("Suite de teste para as Reservas.", function() {
           apartment_id: "02n07j2d1hf5a2f26djjj92a",
           status: "reservado",
           user_id: "6kde3ibi8a1d4187c1ji73bj",
-          date: "2023-08-12T22:49:04.421Z",
           start: "2023-11-12",
           end: "2024-01-12"
         }
@@ -206,6 +198,29 @@ describe("Suite de teste para as Reservas.", function() {
             expect(response.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/3`)
             expect(response.body.RestException.ErrorFields[0].field).toBe('iptClient')
             expect(response.body.RestException.ErrorFields[0].hasError.error).toBe("Nenhum usuário com o ID informado está cadastrado")
+          })
+          .catch(function(error) {
+            fail(error)
+          })
+      })
+
+      test("/PUT - Deve retornar 400, devido a ausência da Data de Início da reserva.", function() {
+        let reserve = {
+          apartment_id: "02n07j2d1hf5a2f26djjj92a",
+          status: "reservado",
+          user_id: "600f191e810c19829de900ea",
+          end: "2024-01-12"
+        }
+        return request.put('/reserves').send(reserve)
+          .then(function(response) {
+            expect(response.statusCode).toEqual(400)
+
+            expect(response.body.RestException.Code).toBe("1")
+            expect(response.body.RestException.Message).toBe("O campo de Data de Início da reserva é obrigatório")
+            expect(response.body.RestException.Status).toBe("400")
+            expect(response.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/1`)
+            expect(response.body.RestException.ErrorFields[0].field).toBe('iptStartDate')
+            expect(response.body.RestException.ErrorFields[0].hasError.error).toBe("O campo de Data de Início da reserva é obrigatório")
           })
           .catch(function(error) {
             fail(error)
