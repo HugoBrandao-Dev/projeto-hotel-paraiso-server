85f4200109b3ed8ab6551ea5548eed66e110fc27
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-25 16:55:33 -0400

    Implementado codigo de atualizacao das informacoes da reserva, no metodo de atualizacao das informacoes.

--- src/models/Reserve.js
+++ src/models/Reserve.js
@@ -5,7 +5,20 @@ const date = new DateFormated('mongodb')
 
 class Reserve {
   async edit(reserve) {
-
+    try {
+      let apartmentIndex = await ApartmentCollection.apartments.data.findIndex(apto => apto.id == reserve.apartment_id)
+      let infos = Object.keys(reserve)
+      for (let info of infos) {
+        if (info != 'apartment_id') {
+          ApartmentCollection.apartments.data[apartmentIndex]["reserve"][info] = reserve[info]
+        }
+      }
+      ApartmentCollection.apartments.data[apartmentIndex]["reserve"].date = date.getDateTime()
+      return
+    } catch (error) {
+      console.log(error)
+      return []
+    }
   }
 }
 
