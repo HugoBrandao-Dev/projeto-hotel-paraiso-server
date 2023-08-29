a034f8c3414201925166e514e89e15ac9b145f30
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-29 18:52:47 -0400

    Modificado teste de SUCESSO na atualizacao das informacoes obrigatorias e opcionais/condicionais com excessao do CPF e da Cidade do usuario, para que verifique se foi retornado o HATEOAS.

--- test/user.test.js
+++ test/user.test.js
@@ -2024,7 +2024,7 @@ describe("Suite de testes das rotas User.", function() {
           })
       })
 
-      test("POST - Deve retornar 200 para usuários Brasileiro que não querem atualizar o CPF e Cidade.", function() {
+      test("PUT - Deve retornar 200 para usuários Brasileiro que não querem atualizar o CPF e Cidade.", function() {
         let user = {
           id: "5da9ea674234635bdff45c02",
           name: "Josias de Oliveira",
@@ -2036,8 +2036,32 @@ describe("Suite de testes das rotas User.", function() {
           birthDate: "1970-06-11"
         }
         return request.put('/users').send(user)
-        .then(function(responsePOST) {
-          expect(responsePOST.statusCode).toEqual(200)
+        .then(function(responsePUT) {
+          expect(responsePUT.statusCode).toEqual(200)
+
+          expect(responsePUT.body._links).toBeDefined()
+            expect(responsePUT.body._links).toHaveLength(4)
+            expect(responsePUT.body._links[0]).toMatchObject({
+              href: `${ baseURL }/users/${ user.id }`,
+              method: 'GET',
+              rel: 'self_user'
+            })
+            expect(responsePUT.body._links[1]).toMatchObject({
+              href: `${ baseURL }/users/${ user.id }`,
+              method: 'PUT',
+              rel: 'edit_user'
+            })
+            expect(responsePUT.body._links[2]).toMatchObject({
+              href: `${ baseURL }/users/${ user.id }`,
+              method: 'DELETE',
+              rel: 'delete_user'
+            })
+            expect(responsePUT.body._links[3]).toMatchObject({
+              href: `${ baseURL }/users`,
+              method: 'GET',
+              rel: 'user_list'
+            })
+
           return request.get(`/users/${ user.id }`)
             .then(function(responseGET) {
               expect(responseGET.statusCode).toEqual(200)
