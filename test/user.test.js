cc6a98fbfb02d0b27a32f3aeaada75edfe9404f2
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-08-29 20:48:15 -0400

    Modificado teste de SUCESSO na atualizacao de Usuario que modificam seu local de nascimento e informa um Numero de Passaporte, para que verifique se foi retornado o HATEOAS.

--- test/user.test.js
+++ test/user.test.js
@@ -2454,7 +2454,7 @@ describe("Suite de testes das rotas User.", function() {
           })
       })
 
-      test("POST - Deve retornar 200 para o usuário Brasileiro que modifica seu local de nascimento e informa o Número do Passaporte.", function() {
+      test("PUT - Deve retornar 200 para o usuário Brasileiro que modifica seu local de nascimento e informa o Número do Passaporte.", function() {
         let user = {
           id: "5da9ea674234635bdff45c02",
           name: "Jeremias de Oliveira",
@@ -2467,6 +2467,29 @@ describe("Suite de testes das rotas User.", function() {
         return request.put('/users').send(user)
           .then(function(responsePUT) {
             expect(responsePUT.statusCode).toEqual(200)
+
+            expect(responsePUT.body._links).toBeDefined()
+            expect(responsePUT.body._links).toHaveLength(4)
+            expect(responsePUT.body._links[0]).toMatchObject({
+              href: `${ baseURL }/users/${ user.id }`,
+              method: 'GET',
+              rel: 'self_user'
+            })
+            expect(responsePUT.body._links[1]).toMatchObject({
+              href: `${ baseURL }/users/${ user.id }`,
+              method: 'PUT',
+              rel: 'edit_user'
+            })
+            expect(responsePUT.body._links[2]).toMatchObject({
+              href: `${ baseURL }/users/${ user.id }`,
+              method: 'DELETE',
+              rel: 'delete_user'
+            })
+            expect(responsePUT.body._links[3]).toMatchObject({
+              href: `${ baseURL }/users`,
+              method: 'GET',
+              rel: 'user_list'
+            })
             return request.get(`/users/${ user.id }`)
               .then(function(responseGET) {
                 const {
