5374d6fd08adb2671afc29d54ff84487dbc71f99
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-09-13 17:00:33 -0400

    Movido funcao de Nivel de Acesso para um arquivo proprio.

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,91 +1,10 @@
 const jwt = require('jsonwebtoken')
-
-const Analyzer = require('../tools/Analyzer')
-const User = require('../models/User')
+const isActionAllowed = require('../tools/accessLevel')
 
 const secret = 'k372gkhcfmhg6l9nj19i51ng'
 
 const projectLinks = {
   errors: 'https://projetohotelparaiso.dev/docs/erros'
-}
-
-// Verifica se a conta tem permissão de realizar uma determinada ação.
-async function isActionAllowed(decodedToken, method, params, body) {
-  try {
-    const upperMethod = method.toUpperCase()
-    let allowed = false
-
-    switch (decodedToken.role) {
-      case '0':
-        switch (upperMethod) {
-          case 'POST':
-            break
-          case 'GET':
-            if (params.id && params.id === decodedToken.id) {
-              allowed = true
-            }
-            break
-          case 'PUT':
-            if (body.id === decodedToken.id) {
-              if (!(body.role >= 0)) {
-                allowed = true
-              }
-            }
-            break
-          case 'DELETE':
-            if (params.id && decodedToken.id === params.id) {
-              allowed = true
-            }
-            break
-        }
-        break
-      case '1':
-        switch (upperMethod) {
-          case 'POST':
-            break
-          case 'GET':
-            break
-          case 'PUT':
-            if (body.role < 1) {
-              allowed = true
-            }
-            break
-          case 'DELETE':
-            // Faz a busca pelo usuário que será deletado
-            let idResult = await Analyzer.analyzeID(params.id)
-            if (!idResult.hasError.value) {
-              let userToBeDeleted = await User.findOne(params.id)
-              if (userToBeDeleted.role < 1) {
-                allowed = true
-              }
-            }
-            break
-        }
-        break
-      case '2':
-        switch (upperMethod) {
-          case 'POST':
-            break
-          case 'GET':
-            break
-          case 'PUT':
-            if (body.role <= 1) {
-              allowed = false
-            }
-            break
-          case 'DELETE':
-            break
-        }
-        break
-      case '4':
-        allowed = true
-        break
-    }
-
-    return allowed
-  } catch (error) {
-    console.log(error)
-  }
 }
 
 function authentication(req, res, next) {
---
+++ src/tools/accessLevel.js
@@ -0,0 +1,100 @@
+const Analyzer = require('../tools/Analyzer')
+const User = require('../models/User')
+
+// Verifica se a conta tem permissão de realizar uma determinada ação.
+async function isActionAllowed(decodedToken, method, params, body) {
+  try {
+    const upperMethod = method.toUpperCase()
+    let allowed = false
+
+    switch (decodedToken.role) {
+      case '0':
+        switch (upperMethod) {
+          case 'POST':
+            break
+          case 'GET':
+
+            // Verifica se o ID passado no parâmetro é o mesmo do armazenado no Token.
+            if (params.id && params.id === decodedToken.id) {
+              allowed = true
+            }
+            break
+          case 'PUT':
+
+            // Verifica se o ID passado no corpo é o mesmo do armazenado no Token.
+            if (body.id === decodedToken.id) {
+
+              // Verifica se o Cliente está tentando alterar sua função.
+              if (!(body.role >= 0)) {
+                allowed = true
+              }
+            }
+            break
+          case 'DELETE':
+
+            // Verifica se o ID passado no parâmetro é o mesmo do armazenado no Token.
+            if (params.id && decodedToken.id === params.id) {
+              allowed = true
+            }
+            break
+        }
+        break
+      case '1':
+        switch (upperMethod) {
+          case 'POST':
+            break
+          case 'GET':
+            break
+          case 'PUT':
+            if (body.role < 1) {
+              allowed = true
+            }
+            break
+          case 'DELETE':
+
+            // Analisa o ID do parâmetro.
+            let idResult = await Analyzer.analyzeID(params.id)
+
+            // Verifica se o ID passado no parâmetro tem algum erro.
+            if (!idResult.hasError.value) {
+
+              // Busca pelo usuário que será deletado.
+              let userToBeDeleted = await User.findOne(params.id)
+
+              // Verifica se a conta a ser deletada é do tipo Cliente (0).
+              if (userToBeDeleted.role < 1) {
+                allowed = true
+              }
+            }
+            break
+        }
+        break
+      case '2':
+        switch (upperMethod) {
+          case 'POST':
+            break
+          case 'GET':
+            break
+          case 'PUT':
+
+            // Verifica se o Gerente está tentando passar um valor de Função menor que o dele (2).
+            if (body.role <= 1) {
+              allowed = true
+            }
+            break
+          case 'DELETE':
+            break
+        }
+        break
+      case '4':
+        allowed = true
+        break
+    }
+
+    return allowed
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = isActionAllowed
