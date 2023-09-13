faea89c40f68c0f73fc9dd2a7417f0c82f378cca
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-09-13 15:35:13 -0400

    Add codigo que impede que um funcionario delete a conta de outro.

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,5 +1,8 @@
 const jwt = require('jsonwebtoken')
 
+const Analyzer = require('../tools/Analyzer')
+const User = require('../models/User')
+
 const secret = 'k372gkhcfmhg6l9nj19i51ng'
 
 const projectLinks = {
@@ -7,66 +10,78 @@ const projectLinks = {
 }
 
 // Verifica se a conta tem permissão de realizar uma determinada ação.
-function isActionAllowed(decodedToken, method, params, body) {
-  const upperMethod = method.toUpperCase()
-  let allowed = true
+async function isActionAllowed(decodedToken, method, params, body) {
+  try {
+    const upperMethod = method.toUpperCase()
+    let allowed = true
 
-  switch (decodedToken.role) {
-    case '0':
-      switch (upperMethod) {
-        case 'POST':
-          allowed = false
-          break
-        case 'GET':
-          if (!params.id || params.id !== decodedToken.id) {
+    switch (decodedToken.role) {
+      case '0':
+        switch (upperMethod) {
+          case 'POST':
             allowed = false
-          }
-          break
-        case 'PUT':
-          if (body.id !== decodedToken.id || body.role > 0) {
-            allowed = false
-          }
-          break
-        case 'DELETE':
-          if (!params.id || decodedToken.id !== params.id) {
-            allowed = false
-          }
-          break
-      }
-      break
-    case '1':
-      switch (upperMethod) {
-        case 'POST':
-          break
-        case 'GET':
-          break
-        case 'PUT':
-          if (body.role > 0) {
-            allowed = false
-          }
-          break
-        case 'DELETE':
-          break
-      }
-      break
-    case '2':
-      switch (upperMethod) {
-        case 'POST':
-          break
-        case 'GET':
-          break
-        case 'PUT':
-          if (body.role > 1) {
-            allowed = false
-          }
-          break
-        case 'DELETE':
-          break
-      }
-      break
+            break
+          case 'GET':
+            if (!params.id || params.id !== decodedToken.id) {
+              allowed = false
+            }
+            break
+          case 'PUT':
+            if (body.id !== decodedToken.id || body.role > 0) {
+              allowed = false
+            }
+            break
+          case 'DELETE':
+            if (!params.id || decodedToken.id !== params.id) {
+              allowed = false
+            }
+            break
+        }
+        break
+      case '1':
+        switch (upperMethod) {
+          case 'POST':
+            break
+          case 'GET':
+            break
+          case 'PUT':
+            if (body.role > 0) {
+              allowed = false
+            }
+            break
+          case 'DELETE':
+            // Faz a busca pelo usuário que será deletado
+            let idResult = await Analyzer.analyzeID(params.id)
+            if (!idResult.hasError.value) {
+              let userToBeDeleted = await User.findOne(params.id)
+              if (userToBeDeleted.role > 0) {
+                allowed = false
+              }
+            }
+            break
+        }
+        break
+      case '2':
+        switch (upperMethod) {
+          case 'POST':
+            break
+          case 'GET':
+            break
+          case 'PUT':
+            if (body.role > 1) {
+              allowed = false
+            }
+            break
+          case 'DELETE':
+            break
+        }
+        break
+    }
+
+    return allowed
+  } catch (error) {
+    console.log(error)
   }
-
-  return allowed
 }
 
 function authentication(req, res, next) {
@@ -78,19 +93,25 @@ function authentication(req, res, next) {
       if (error) {
         console.log(error)
       } else {
-        if (isActionAllowed(decoded, req.method, req.params, req.body)) {
-          next()
-        } else {
-          let RestException = {
-            Code: '6',
-            Message: 'O usuário não está autenticado',
-            Status: '403',
-            MoreInfo: `${ projectLinks.errors }/6`
-          }
+        isActionAllowed(decoded, req.method, req.params, req.body)
+          .then(function(responseAllowed) {
+            if (responseAllowed) {
+              next()
+            } else {
+              let RestException = {
+                Code: '6',
+                Message: 'O usuário não está autenticado',
+                Status: '403',
+                MoreInfo: `${ projectLinks.errors }/6`
+              }
 
-          res.status(parseInt(RestException.Status))
-          res.json({ RestException })
-        }
+              res.status(parseInt(RestException.Status))
+              res.json({ RestException })
+            }
+          })
+          .catch(function(errorAllowed) {
+
+          })
       }
     })
   } catch (error) {
