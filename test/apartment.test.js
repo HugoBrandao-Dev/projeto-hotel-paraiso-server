24fd5d83c5d143eef929e3a282c78758cc2efd4c
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-09-18 15:27:05 -0400

    Add token Admin no teste FALHO devido a ausencia de algum campo no Comodo.

--- test/apartment.test.js
+++ test/apartment.test.js
@@ -783,8 +783,8 @@ describe("Suite de testes das rotas de Apartment.", function() {
 
       })
 
-      /*
       test("/POST - Deve retornar 400, pela ausência de um campo no cômodo do apartamento.", function() {
+
         let apartment = {
           floor: "3",
           number: "10",
@@ -795,22 +795,27 @@ describe("Suite de testes das rotas de Apartment.", function() {
           ],
           daily_price: '200'
         }
-        return request.post(endpoints.toCreate).send(apartment)
-          .then(function(response) {
-            expect(response.statusCode).toEqual(400)
 
-            expect(response.body.RestException.Code).toBe("2")
-            expect(response.body.RestException.Message).toBe('Faltam campos em um dos cômodos informados')
-            expect(response.body.RestException.Status).toBe("400")
-            expect(response.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/2`)
-            expect(response.body.RestException.ErrorFields[0].field).toBe('iptRooms')
-            expect(response.body.RestException.ErrorFields[0].hasError.error).toBe('Faltam campos em um dos cômodos informados')
+        return request.post(endpoints.toCreate).send(apartment).set('Authorization', accounts.admin.token)
+          .then(function(responseCreate) {
+
+            expect(responseCreate.statusCode).toEqual(400)
+
+            expect(responseCreate.body.RestException.Code).toBe("2")
+            expect(responseCreate.body.RestException.Message).toBe('Faltam campos em um dos cômodos informados')
+            expect(responseCreate.body.RestException.Status).toBe("400")
+            expect(responseCreate.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/2`)
+            expect(responseCreate.body.RestException.ErrorFields[0].field).toBe('iptRooms')
+            expect(responseCreate.body.RestException.ErrorFields[0].hasError.error).toBe('Faltam campos em um dos cômodos informados')
+
           })
-          .catch(function(error) {
-            fail(error)
+          .catch(function(errorCreate) {
+            fail(errorCreate)
           })
+
       })
 
+      /*
       test("/POST - Deve retornar 400, devido a presença de caracteres inválidos na quantidade de um determinado comodo.", function() {
         let apartment = {
           floor: "3",
