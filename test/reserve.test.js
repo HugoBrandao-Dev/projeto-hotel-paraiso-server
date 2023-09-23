5fa1d5d21db7a767ac7e03a28fd1babe9077b844
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-09-22 23:53:52 -0400

    Criado teste FALHO para cadastro de uma reserva, a Data de Inicio contem caracteres invalidos.

--- test/reserve.test.js
+++ test/reserve.test.js
@@ -396,7 +396,8 @@ describe("Suite de teste para as Reservas.", function() {
 
       })
 
-      test("/POST - Deve retornar 400, a data de início da reserva não foi informada.", function() {
+      // Testes de Data de Início da reserva.
+      test("/POST - Deve retornar 400, a Data de Início da reserva não foi informada.", function() {
 
         let reserve = {
           apartment_id: "02n07j2d1hf5a2f26djjj92a",
@@ -414,6 +415,31 @@ describe("Suite de teste para as Reservas.", function() {
             expect(responseCreate.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/1`)
             expect(responseCreate.body.RestException.ErrorFields[0].field).toBe('iptStartDate')
             expect(responseCreate.body.RestException.ErrorFields[0].hasError.error).toBe("O campo de Data de Início da reserva é obrigatório")
+
+          })
+          .catch(function(errorCreate) {
+            fail(errorCreate)
+          })
+
+      })
+
+      test("/POST - Deve retornar 400, a Data de Início da reserva não foi informada.", function() {
+
+        let reserve = {
+          apartment_id: "02n07j2d1hf5a2f26djjj92a",
+          start: "2023-11-0*",
+          end: "2024-01-12"
+        }
+
+        return request.post(endpoints.toCreate).send(reserve).set('Authorization', accounts.cliente.token)
+          .then(function(responseCreate) {
+
+            expect(responseCreate.body.RestException.Code).toBe("2")
+            expect(responseCreate.body.RestException.Message).toBe("O campo de Data de Início da reserva possui caracteres inválidos")
+            expect(responseCreate.body.RestException.Status).toBe("400")
+            expect(responseCreate.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/2`)
+            expect(responseCreate.body.RestException.ErrorFields[0].field).toBe('iptStartDate')
+            expect(responseCreate.body.RestException.ErrorFields[0].hasError.error).toBe("O campo de Data de Início da reserva possui caracteres inválidos")
 
           })
           .catch(function(errorCreate) {
