9df56a81a7d12a10331e9a09ab678c080373c4ab
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-09-27 18:37:09 -0400

    Retirado teste FALHO na atualizacao de um Reserva devido a ausencia de Status.

--- test/reserve.test.js
+++ test/reserve.test.js
@@ -1851,38 +1851,6 @@ describe("Suite de teste para as Reservas.", function() {
       })
 
       // Falhas no Status da reserva.
-      test("/PUT - Deve retornar 400, uma vez que NÃO foi informado o Status.", function() {
-
-        let start = dateNow.getDate()
-        let end = getDateWithNextMonth(start)
-
-        let reserve = {
-          apartment_id: "856377c88f8fd9fc65fd3ef5",
-          status: "",
-          user_id: accounts.cliente.id,
-          start,
-          end,
-        }
-
-        return request.put(endpoints.toUpdate).send(reserve).set('Authorization', accounts.funcionario.token)
-          .then(function(responseUpdate) {
-
-            expect(responseUpdate.statusCode).toEqual(400)
-
-            expect(responseUpdate.body.RestException.Code).toBe("1")
-            expect(responseUpdate.body.RestException.Message).toBe("O campo de Status é obrigatório")
-            expect(responseUpdate.body.RestException.Status).toBe("400")
-            expect(responseUpdate.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/1`)
-            expect(responseUpdate.body.RestException.ErrorFields[0].field).toBe('iptStatus')
-            expect(responseUpdate.body.RestException.ErrorFields[0].hasError.error).toBe('O campo de Status é obrigatório')
-
-          })
-          .catch(function(errorUpdate) {
-            fail(errorUpdate)
-          })
-
-      })
-
       test("/PUT - Deve retornar 400, já que o valor de Status é inválido.", function() {
 
         let start = dateNow.getDate()
