0193c5b16ba4e056b4b920d3bf7ec6c751791d43
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-09-27 20:13:49 -0400

    Add teste FALHO na atualizacao de uma reserva, a Data de Inicio contem caracteres invalidos.

--- test/reserve.test.js
+++ test/reserve.test.js
@@ -2011,31 +2011,39 @@ describe("Suite de teste para as Reservas.", function() {
 
       })
 
-      /*
       test("/PUT - Deve retornar 400, devido a presença de caracteres inválidos na Data de Início da reserva.", function() {
-        let reserve = {
-          apartment_id: "02n07j2d1hf5a2f26djjj92a",
-          status: "reservado",
-          user_id: "600f191e810c19829de900ea",
-          start: "2023-12-*2",
-          end: "2024-01-12"
-        }
-        return request.put(endpoints.toUpdate).send(reserve)
-          .then(function(response) {
-            expect(response.statusCode).toEqual(400)
 
-            expect(response.body.RestException.Code).toBe("2")
-            expect(response.body.RestException.Message).toBe("O campo de Data de Início da reserva possui caracteres inválidos")
-            expect(response.body.RestException.Status).toBe("400")
-            expect(response.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/2`)
-            expect(response.body.RestException.ErrorFields[0].field).toBe('iptStartDate')
-            expect(response.body.RestException.ErrorFields[0].hasError.error).toBe("O campo de Data de Início da reserva possui caracteres inválidos")
+        let start = dateNow.getDate()
+        let end = getDateWithNextMonth(start)
+
+        let reserve = {
+          apartment_id: "27ibm1he7gl4ei9i7jcacbl6",
+          status: "ocupado",
+          user_id: accounts.cliente.id,
+          start: start.slice(0, -1) + '*',
+          end,
+        }
+
+        return request.put(endpoints.toUpdate).send(reserve).set('Authorization', accounts.funcionario.token)
+          .then(function(responseUpdate) {
+
+            expect(responseUpdate.statusCode).toEqual(400)
+
+            expect(responseUpdate.body.RestException.Code).toBe("2")
+            expect(responseUpdate.body.RestException.Message).toBe("A Data de Início escolhida é inválida")
+            expect(responseUpdate.body.RestException.Status).toBe("400")
+            expect(responseUpdate.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/2`)
+            expect(responseUpdate.body.RestException.ErrorFields[0].field).toBe('iptStartDate')
+            expect(responseUpdate.body.RestException.ErrorFields[0].hasError.error).toBe("A Data de Início escolhida é inválida")
+
           })
-          .catch(function(error) {
-            fail(error)
+          .catch(function(errorUpdate) {
+            fail(errorUpdate)
           })
+
       })
 
+      /*
       test("/PUT - Deve retornar 400, já que a Data de Início é anterior a data Atual.", function() {
         let reserve = {
           apartment_id: "02n07j2d1hf5a2f26djjj92a",
