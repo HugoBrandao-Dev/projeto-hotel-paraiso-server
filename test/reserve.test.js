c53d46464488a6d452ea1aaa6c76aa6e03d2bf97
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-10-02 22:59:25 -0400

    Add teste FALHO de listagem de reservas, a terceira query e invalida.

--- test/reserve.test.js
+++ test/reserve.test.js
@@ -2073,6 +2073,31 @@ describe("Suite de teste para as Reservas.", function() {
 
       })
 
+      test("/GET - Deve retornar 400, informado o parâmetro de consulta inválido, como a terceira query string.", function() {
+
+        let queryString = {
+          status: 'livre',
+          offset: 2,
+          limit: 2
+        }
+
+        return request.get(`${ endpoints.toList }?status=livre&offset=${ queryString.offset }&limi=${ queryString.limit }`).set('Authorization', accounts.cliente.token)
+          .then(function(responseList) {
+
+            expect(responseList.statusCode).toEqual(400)
+
+            expect(responseList.body.RestException.Code).toBe("2")
+            expect(responseList.body.RestException.Message).toBe("O parâmetro \'limi\' é inválido")
+            expect(responseList.body.RestException.Status).toBe("400")
+            expect(responseList.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/2`)
+
+          })
+          .catch(function(errorList) {
+            fail(errorList)
+          })
+
+      })
+
     })
 
   })
