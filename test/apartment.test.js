6b6605ef73aee89cfa279f97d84f80c2144a667d
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-11-07 15:21:56 -0400

    Add teste para FALHA na leitura, o valor de offset e negativo.

--- test/apartment.test.js
+++ test/apartment.test.js
@@ -2602,6 +2602,27 @@ describe("Suite de testes das rotas de Apartment.", function() {
 
       })
 
+      test("/GET - Deve retornar 400, o valor do offset é inválido por ser negativo.", function() {
+
+        let url = endpoints.toList + '?offset=-2&limit=3'
+
+        return request.get(url).set('Authorization', accounts.cliente.token)
+          .then(function(responseList) {
+
+            expect(responseList.statusCode).toEqual(400)
+
+            expect(responseList.body.RestException.Code).toBe('2')
+            expect(responseList.body.RestException.Message).toBe('O valor do parâmetro Offset é inválido')
+            expect(responseList.body.RestException.Status).toBe('400')
+            expect(responseList.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/2`)
+
+          })
+          .catch(function(errorRead) {
+            fail(errorRead)
+          })
+
+      })
+
       test("/GET - Deve retornar 404, o valor do offset é maior que o último apto cadastrado.", function() {
 
         let url = endpoints.toList + '?offset=2000&limit=3'
