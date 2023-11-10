ab84537922b3017c794de3eaad6f62029082dcbd
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-11-10 17:05:31 -0400

    Emitindo erro para quando o cliente informar o status.

--- test/apartment.test.js
+++ test/apartment.test.js
@@ -2567,6 +2567,27 @@ describe("Suite de testes das rotas de Apartment.", function() {
 
       })
 
+      test("/GET - Deve retornar 400, o cliente (logado) não pode usar o parâmetro status.", function() {
+
+        let url = endpoints.toList + '?status='
+
+        return request.get(url).set('Authorization', accounts.cliente.token)
+          .then(function(responseList) {
+
+            expect(responseList.statusCode).toEqual(400)
+
+            expect(responseList.body.RestException.Code).toBe('2')
+            expect(responseList.body.RestException.Message).toBe('O parâmetro \'status\' é inválido')
+            expect(responseList.body.RestException.Status).toBe('400')
+            expect(responseList.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/2`)
+
+          })
+          .catch(function(errorRead) {
+            fail(errorRead)
+          })
+
+      })
+
       test("/GET - Deve retornar 400, o rooms (quantidade de cômodos) foi declarado, mas não possui valor.", function() {
 
         let url = endpoints.toList + '?rooms='
