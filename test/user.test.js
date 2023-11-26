cf640c758985d27e839912c199e7e3c1b621c3d8
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-11-25 20:25:50 -0400

    Add teste que impede a listagem caso o offset tenha sido passado sem o limit.]

--- test/user.test.js
+++ test/user.test.js
@@ -2142,6 +2142,27 @@ describe("Suite de testes das rotas User.", function() {
           })
       })
 
+      test("/POST - Deve retornar 400, o filtro Offset foi informado sem a presença do Limit.", function() {
+
+        const queryStringOBJ = {
+          offset: '1'
+        }
+
+        const url = endpoints.toList + Generator.genQueryStringFromObject(queryStringOBJ)
+
+        return request.get(url).set('Authorization', accounts.funcionario.token)
+          .then(function(responseList) {
+            expect(responseList.statusCode).toEqual(400)
+            expect(responseList.body.RestException.Code).toBe("1")
+            expect(responseList.body.RestException.Message).toBe("O parâmetro OFFSET foi informado sem a presença do LIMIT")
+            expect(responseList.body.RestException.Status).toBe("400")
+            expect(responseList.body.RestException.MoreInfo).toBe(`${ projectLinks.errors }/1`)
+          })
+          .catch(function(errorList) {
+            fail(errorList)
+          })
+      })
+
       // Testes para busca por documentação
       test("/POST - Deve retornar 401, por tentar buscar um Usuários pelo CPF sem estar AUTORIZADO.", function() {
 
